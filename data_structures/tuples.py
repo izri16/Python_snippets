972c7869755fe08f23437d6e91330d4aa64424e3
richard <richardizip@gmail.com>
2016-02-10 12:31:29 +0100

    update: add tuple notes

---
+++ data_structures/tuples.py
@@ -0,0 +1,16 @@
+# tuple with one item must have trailing comma
+# and is created without braces
+one_item_tupple = 7,
+
+# tuple with more items has paranthesis
+tup = (1,2,3)
+x, _, z = tup
+
+# empty tuple
+empty_tup = ()
+
+# convert list to tuple
+l = [1,2,3]
+t = tuple(l)
+print(t)
+

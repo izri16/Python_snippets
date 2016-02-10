d5124c2aebf283e14e0b5b3cbdd98d5a50f04dab
richard <richardizip@gmail.com>
2016-02-10 14:01:40 +0100

    update: add dictonary info

---
+++ data_structures/dicts.py
@@ -0,0 +1,74 @@
+'''
+Use 'in' to check if key exists in dictonary.
+
+Mutable object can not be used as keys.
+'''
+
+my_dict = {}
+my_dict['name'] = 'Richard'
+del my_dict['name']
+del my_dict
+
+my_dict = {'name':'Richard', 'age':21, 2:1234}
+
+# when the keys are strings
+my_dict = dict(name='Richard', age=21) 
+
+# when keys are arbitrary
+my_dict = dict([('name', 'Richard'),('age',21),(2,1234)])
+
+print(my_dict.items())
+print(my_dict.keys())
+print(my_dict.values())
+
+my_dict_copy = my_dict.copy()
+my_dict.clear()
+
+'''
+Usefull applications.
+'''
+
+array = [1,4,7,2,8,9,5,4,4,7]
+
+'''
+Check if there are two same items in array in expected time O(n).
+TOTO SA DA VELMI ZJEDNODUSIT ...
+'''
+
+hash_set = {}
+for item in array:
+    if (item in hash_set):
+        print("same")
+        break
+    else:
+        hash_set[item] = 1
+
+'''
+Check if there are arbitrary many same items in array.
+'''
+hash_set = {}
+count = 3
+for item in array:
+    if (item in hash_set):
+        if (hash_set[item] >= count - 1):
+            print("same")
+            break
+        else:
+            hash_set[item] += 1
+    else:
+        hash_set[item] = 1
+
+
+'''
+For every item in array find how many times it appears in O(n).
+'''
+
+stats = {}
+for item in array:
+    if (item in stats):
+        stats[item] += 1
+    else:
+        stats[item] = 1
+
+print(stats)
+
